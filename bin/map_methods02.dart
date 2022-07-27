00fcc48a9bf9bea5714a471f835283b173dc65c0
ogabek <norpulatovogabek42@gmail.com>
2022-07-27 11:29:54 +0500

    sdfs

--- bin/map_methods02.dart
+++ bin/map_methods02.dart
@@ -2,10 +2,12 @@
 Car details will be given to you in map view, change the car price for it
 */
 
-void main() {}
+void main() {
+  print(func({'name': 'Mustang', 'color': 'silver', 'price': 6000}, 4500));
+}
 
 Map func(Map data, int cost) {
+  data["price"] = cost;
   //your code here
-
   return data;
 }
--- bin/map_methods05.dart
+++ bin/map_methods05.dart
@@ -4,9 +4,14 @@ the key in it is last name,
 change it to surname
 */
 
-void main() {}
+void main() {
+  print(func({'name': 'Mustang', 'last_name': 'silver'}));
+}
 
 Map func(Map data) {
+  String w = data['last_name'];
+  data.remove('last_name');
+  data["surname"] = w;
   //your code here
 
   return data;
